76c793aff4c5761b6ef613a133f29115e62357ac
Pasit Sakorn <pasit.sakorn@gmail.com>
2024-04-30 11:57:06 +0700

    Added explainations for javascript file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Alphabetical months for conversion
 const months = [
   'January',
   'February',
@@ -13,6 +14,7 @@ const months = [
   'December',
 ];
 
+// Alphabetical days for conversion
 const days = [
   'Sunday',
   'Monday',
@@ -23,6 +25,7 @@ const days = [
   'Saturday',
 ];
 
+// Periods of each day sorted respectively
 const periods = {
   monday: {
     0: 'homeroom_1',
@@ -74,47 +77,34 @@ const periods = {
     9: 'ง22101_1',
   },
   friday: {
-    0: 'homeroom_1', // A
-    1: 'พ22102_1', // 1
-    2: 'ท22101_1', // 2
-    3: 'break_1', // Break
-    4: 'อ22102_2', // 3
-    5: 'lunch_1', // 4
-    6: 'ว22101_3', // 5
-    7: 'ค22101_2', // 6
-    8: 'อ20223_3', // 7
-    9: 'ส22103_1', // 8
+    0: 'homeroom_1',
+    1: 'พ22102_1',
+    2: 'ท22101_1',
+    3: 'break_1',
+    4: 'อ22102_2',
+    5: 'lunch_1',
+    6: 'ว22101_3',
+    7: 'ค22101_2',
+    8: 'อ20223_3',
+    9: 'ส22103_1',
   },
 };
 
 const times = {
-  0: hmToMs(7, 25),
-  1: hmToMs(8, 15),
-  2: hmToMs(9, 5),
-  3: hmToMs(10, 10),
-  4: hmToMs(11, 0),
-  5: hmToMs(11, 50),
-  6: hmToMs(12, 40),
-  7: hmToMs(13, 30),
-  8: hmToMs(14, 20),
-  9: hmToMs(15, 10),
-  10: hmToMs(17, 50),
+  0: hmToMs(7, 25), // Period A (homeroom)
+  1: hmToMs(8, 15), // 1st Period
+  2: hmToMs(9, 5), // 2nd Period
+  3: hmToMs(10, 10), // Break
+  4: hmToMs(11, 0), // 3rd Period
+  5: hmToMs(11, 50), // Lunch
+  6: hmToMs(12, 40), // 4th Period
+  7: hmToMs(13, 30), // 5th Period
+  8: hmToMs(14, 20), // 6th Period
+  9: hmToMs(15, 10), // 7th Period
+  10: hmToMs(17, 50), // 8th Period (max for every day)
 };
 
-const timesTest = {
-  0: hmToMs(7, 25), // A
-  1: hmToMs(8, 15), // 1
-  2: hmToMs(9, 5), // 2
-  3: hmToMs(9, 55), // Break
-  4: hmToMs(10, 10), // 3
-  5: hmToMs(11, 0), // Lunch
-  6: hmToMs(11, 50), // 5
-  7: hmToMs(12, 40), // 6
-  8: hmToMs(13, 30), // 7
-  9: hmToMs(14, 20), // 8
-  10: hmToMs(15, 10), // 9
-};
-
+// Period names
 const periodCodesNames = {
   homeroom: 'A',
   รักการอ่าน: 'รักการอ่าน',
@@ -124,6 +114,7 @@ const periodCodesNames = {
   แนะแนว: 'แนะแนว',
   break: 'Break',
 
+  // 1st Semester
   ท22101: 'ภาษาไทยพื้นฐาน 3',
   ค22101: 'คณิตศาสตร์พื้นฐาน 3',
   ว22101: 'วิทยาศาสตร์พื้นฐาน 4',
@@ -143,6 +134,7 @@ const periodCodesNames = {
   จ20201: 'ภาษาจีนเพื่อความเพลิดเพลิน 2',
   ญ20201: 'ภาษาญี่ปุ่นเพื่อความเพลิดเพลิน 2',
 
+  // 2nd Semester
   ท21102: 'ภาษาไทยพื้นฐาน 4',
   ค22102: 'คณิตศาสตร์พื้นฐาน 4',
   ว22102: 'วิทยาศาสตร์พื้นฐาน 4',
@@ -162,6 +154,7 @@ const periodCodesNames = {
   ญ20202: 'ภาษาญี่ปุ่นเพื่อความเพลิดเพลิน 2',
 };
 
+// Period names (simplified version)
 const periodCodesNamesSimplified = {
   homeroom: 'A',
   รักการอ่าน: 'รักการอ่าน',
@@ -171,6 +164,7 @@ const periodCodesNamesSimplified = {
   แนะแนว: 'แนะแนว',
   break: 'Break',
 
+  // 1st Semester
   ท22101: 'Thai',
   ค22101: 'Maths',
   ว22101: 'Science',
@@ -190,6 +184,7 @@ const periodCodesNamesSimplified = {
   จ20201: 'Chinese',
   ญ20201: 'Japanese',
 
+  // 2nd Semester
   ท21102: 'Thai',
   ค22102: 'Maths',
   ว22102: 'Science',
@@ -209,6 +204,7 @@ const periodCodesNamesSimplified = {
   ญ20202: 'Japanese',
 };
 
+// List of teachers who teach for each subject
 const periodTeachers = {
   homeroom: '-',
   รักการอ่าน_1: '-',
@@ -218,6 +214,7 @@ const periodTeachers = {
   แนะแนว_1: 'Unknown',
   break_1: '-',
 
+  // 1st Semester
   ท22101_1: 'ครูนวีณา',
   ค22101_1: 'ครูณัฐภัทร',
   ค22101_2: 'T. Jay',
@@ -241,6 +238,7 @@ const periodTeachers = {
   จ20201_1: 'ภาษาจีนเพื่อความเพลิดเพลิน 2',
   ญ20201_1: 'ครูวาวุฒิ/อ.นาราวดี',
 
+  // 2nd Semester
   ท21102_1: 'ครูนวีณา',
   ค22102: 'คณิตศาสตร์พื้นฐาน 4',
   ว22102: 'วิทยาศาสตร์พื้นฐาน 4',
@@ -260,57 +258,68 @@ const periodTeachers = {
   ญ20202: 'ภาษาญี่ปุ่นเพื่อความเพลิดเพลิน 2',
 };
 
-const timeE = document.getElementById('time');
-const noticeE = document.getElementById('notice');
-const contentsE = document.getElementById('contents');
+// Other elements
+const timeE = document.getElementById('time'); // Element for displaying time
+const noticeE = document.getElementById('notice'); // Element for displaying certain things
+const contentsE = document.getElementById('contents'); // Whole webpage (excluding notice and progress bar)
 
-const startTimeE = document.getElementById('startTime');
-const endTimeE = document.getElementById('endTime');
-const periodNameE = document.getElementById('periodName');
-const periodCodeE = document.getElementById('periodCode');
-const teachersE = document.getElementById('teachers');
-const timeElapsedE = document.getElementById('timeElapsed');
+// Elements for displaying current subject's information
+const startTimeE = document.getElementById('startTime'); // Element for displaying starting time for current subject
+const endTimeE = document.getElementById('endTime'); // Element for displaying ending time for current subject
+const periodNameE = document.getElementById('periodName'); // Element for displaying current subject's name
+const periodCodeE = document.getElementById('periodCode'); // Element for displaying current subject's code
+const teachersE = document.getElementById('teachers'); // Element for displaying current subject's teachers
+const timeElapsedE = document.getElementById('timeElapsed'); // Element for displaying time elapsed since current subject starts
 
-const startTimeE2 = document.getElementById('startTime2');
-const endTimeE2 = document.getElementById('endTime2');
-const periodNameE2 = document.getElementById('periodName2');
-const periodCodeE2 = document.getElementById('periodCode2');
-const teachersE2 = document.getElementById('teachers2');
-const timeTillE = document.getElementById('timeTill2');
-const progressInsideE = document.getElementById('progressInside');
+// Elements for displaying next subject's information
+const startTimeE2 = document.getElementById('startTime2'); // Element for displaying starting time for next subject
+const endTimeE2 = document.getElementById('endTime2'); // Element for displaying ending time for next subject
+const periodNameE2 = document.getElementById('periodName2'); // Element for displaying next subject's name
+const periodCodeE2 = document.getElementById('periodCode2'); // Element for displaying next subject's code
+const teachersE2 = document.getElementById('teachers2'); // Element for displaying next subject's teachers
+const timeTillE = document.getElementById('timeTill2'); // Element for displaying time till the next subject starts
+const progressInsideE = document.getElementById('progressInside'); // Progress bar (the part inside)
 
-const useSimplifiedPeriodNames = true;
+const useSimplifiedPeriodNames = true; // Use easy-to-understand subject names than original
 
-setInterval(update, 1000);
+setInterval(update, 1000); // Update data every second
 
+// Function to update data and time
 function update() {
-  const d = new Date();
+  const d = new Date(); // Data about current day
 
-  let day = d.getDay();
-  let dayOfWeek = days[day];
-  let dayOfMonth = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-  let time = d.toLocaleTimeString('en-GB');
+  let day = d.getDay(); // Get days in numbers. 0 = Sunday, 1 = Monday ... 6 = Saturday
+  let dayOfWeek = days[day]; // Transform numerical days to alphabetical. 0 -> Sunday
+  let dayOfMonth = d.getDate(); // Get day of month (1 - 28/29/30/31)
+  let month = months[d.getMonth()]; // Transform numerical months (0-11 // 0 = January ... 11 = December) to alphabetical. 1 -> February
+  let year = d.getFullYear(); // Get current year
+  let time = d.toLocaleTimeString('en-GB'); // Get current time
 
-  let msSinceDay = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-  let ms = d.getTime() - msSinceDay.getTime();
+  let msSinceDayFull = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  let ms = d.getTime() - msSinceDayFull.getTime(); // Gets milliseconds counting since midnight
 
-  let referenceTimes = timesTest;
+  let referenceTimes = times; // Selected subject times data
 
-  if (day === 1 || day === 6) {
+  // Checks if current day is weekends (0 = Sunday, 6 = Saturday)
+  if (day === 0 || day === 6) {
     contentsE.hidden = true;
     noticeE.textContent = `No school today, it's ${days[day]}!`;
 
     return;
-  } else if (ms < referenceTimes[0]) {
+  }
+
+  // Checks if school starts yet
+  else if (ms < referenceTimes[0]) {
     contentsE.hidden = true;
     noticeE.textContent = `School starts at ${
       msToHms(referenceTimes[0]).fullHrsMins
     }! About in ${msToHms(referenceTimes[0] - ms).fullChars}`;
 
     return;
-  } else if (
+  }
+
+  // Checks if school is already over
+  else if (
     ms > referenceTimes[Object.entries(periods[dayOfWeek.toLowerCase()]).length]
   ) {
     contentsE.hidden = true;
@@ -323,9 +332,9 @@ function update() {
     dayOfMonth
   )} ${year}, ${time}`;
 
+  // Period number
   let period = 0;
 
-  //if (d.getDay() !== 0 && d.getDay() !== 6) {
   for (let i = 0; i < Object.keys(referenceTimes).length; i++) {
     if (between(ms, referenceTimes[i], referenceTimes[i + 1])) {
       period = parseInt(getKeyByValue(referenceTimes, referenceTimes[i]));
@@ -334,21 +343,25 @@ function update() {
   }
 
   let referencePeriodNames = periodCodesNames;
+
   if (useSimplifiedPeriodNames)
     referencePeriodNames = periodCodesNamesSimplified;
 
+  // Gets and displays period code for current subject
   let periodCode = periods[dayOfWeek.toLowerCase()][period];
   let periodName =
     referencePeriodNames[
       String(periodCode).substring(0, String(periodCode).length - 2)
     ];
 
+  // Gets and displays period code for next subject
   let periodCode2 = periods[dayOfWeek.toLowerCase()][period + 1];
   let periodName2 =
     referencePeriodNames[
       String(periodCode2).substring(0, String(periodCode2).length - 2)
     ];
 
+  // Removes "_x" suffix
   if (periodCode[periodCode.length - 2] === '_') {
     periodCodeE.textContent = periodCode.substring(
       0,
@@ -358,6 +371,7 @@ function update() {
     periodCodeE.textContent = periodCode;
   }
 
+  // Removes "_x" suffix (for next subject)
   if (periodCode2[periodCode2.length - 2] === '_') {
     periodCodeE2.textContent = periodCode2.substring(
       0,
@@ -367,31 +381,38 @@ function update() {
     periodCodeE2.textContent = periodCode2;
   }
 
+  // Gets and displays teachers who teach for current subject
   periodNameE.textContent = periodName;
   teachersE.textContent = periodTeachers[periodCode];
 
+  // Gets and displays teachers who teach for next subject
   periodNameE2.textContent = periodName2;
   teachersE2.textContent = periodTeachers[periodCode2];
 
+  // Gets and displays the subject's starting and ending time for current subject
   startTimeE.textContent = msToHms(timesTest[period]).fullHrsMins;
   endTimeE.textContent = msToHms(timesTest[period + 1]).fullHrsMins;
 
+  // Gets and displays the subject's starting and ending time for next subject
   startTimeE2.textContent = msToHms(timesTest[period + 1]).fullHrsMins;
   endTimeE2.textContent = msToHms(timesTest[period + 2]).fullHrsMins;
 
+  // Gets and displays time elapsed for current subject
   let timeElapsedMs = ms - referenceTimes[period];
   timeElapsedE.textContent = msToHms(timeElapsedMs).fullMinsSecs;
 
+  // Gets and displays time till the next subject
   let timeTillMs = referenceTimes[period + 1] - ms;
   timeTillE.textContent = msToHms(timeTillMs).fullMinsSecs;
 
+  // Updates the progress bar of the current subject
   progressInsideE.style.width = `${
     (timeElapsedMs / (referenceTimes[period + 1] - referenceTimes[period])) *
     100
   }%`;
 }
 
-// Get the object key name by value
+// Get the object key name by using value
 function getKeyByValue(object, value) {
   return Object.keys(object).find((key) => object[key] === value);
 }
